26b9c2c0803130820899783fe17de1a7666c213f
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-10-29 17:49:46 -0600

    alot of changes

---
+++ client/src/components/Assessment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Assessment = () =>{
+return (
+    <div> 
+        <h3> Resources</h3>
+    </div>
+);
+}
+
+
+export default Assessment;
---
+++ client/src/components/Resources.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resources = () => {
+    return (
+        <div>
+            <h3>Resources</h3>
+        </div>
+    );
+}
+
+export default Resources;
+
