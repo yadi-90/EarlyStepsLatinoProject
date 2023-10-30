83fb64ed11420379965293f34c62b17c12a98476
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-10-29 21:17:26 -0600

    added instructions to assessment page

--- client/src/components/Assessment.js
+++ client/src/components/Assessment.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
-
-const Assessment = () =>{
-return (
-    <div class="assessmenttitle"> 
+const Assessment = () => {
+  return (
+    <div>
+      <div className="assessmenttitle"> 
         <h3> Assessment</h3>
+      </div>
+      <div>
+        <br></br>
+        <br></br>
+        <div>
+          <p>
+            Thank you for taking the time to complete the Early Developmental Assessment. This assessment is designed to help you gain insights into your child's developmental milestones and identify any areas that may warrant further attention or support. Your participation is crucial in providing the best possible care for your child.
+          </p>
+        </div>
+      </div>
     </div>
-    <div>
-    <br></br>
-    <br></br>
-    <div>
-        <p>
-        Thank you for taking the time to complete the Early Developmental Assessment. This assessment is designed to help you gain insights into your child's developmental milestones and identify any areas that may warrant further attention or support. Your participation is crucial in providing the best possible care for your child.
-        </p>
-    </div>
-    </div>
-);
+  );
 }
 
-
 export default Assessment;
+
