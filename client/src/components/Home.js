96b31491ecca89f46f48ce9a58e0cef0c12a6d88
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-11-22 12:18:00 -0700

    made test for autho nav componnet

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
      {location.pathname === '/' ? (
-       <img className="mainimage" src="https://images.unsplash.com/photo-1616543791139-fdca422b7ef7?auto=format&fit=crop&q=80&w=2787&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="family" />
+       <img className="mainimage" src="https://images.unsplash.com/photo-1616543791139-fdca422b7ef7?auto=format&fit=crop&q=80&w=2787&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Happy Latino family enjoying time together."  />
      ) : null}
 <div className="maincontainer">
        
